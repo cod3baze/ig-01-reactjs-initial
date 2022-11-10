3f2f05770abd93db4e2d5ad179cb63fb45a43810
elias alexandre <elliasallex@gmail.com>
2022-11-10 13:37:35 -0300

    header component & app configs

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+import { Post } from "./components/Post";
+
+import "./styles/global.css";
+import styles from "./App.module.css";
+
 export function App() {
   return (
     <div className="App">
-      <h1>Elias alexandre</h1>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          <Post
+            author="Elias Alexandre"
+            content="Programmer focused on tech around JS language, like Typescript,React,NodeJS"
+          />
+          <Post
+            author="Elias Alexandre"
+            content="Programmer focused on tech around JS language, like Typescript,React,NodeJS"
+          />
+        </main>
+      </div>
     </div>
   );
 }
-
